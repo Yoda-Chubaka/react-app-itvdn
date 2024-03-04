88ac252c225ef5c9cd1bb6374e8803d2bcc622c5
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-03-04 18:06:14 +0200

    Init

---
+++ project1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>There is main article about React App. Enjoy it!</p>
+    </div>
+  );
+}
+
+export default App;
